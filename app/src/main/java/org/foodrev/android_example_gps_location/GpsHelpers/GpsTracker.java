66170dadfbf6a882dd142b86fa7690eb1c1f7b16
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-10-02 12:35:07 -0700

    added empty singleton class

---
+++ app/src/main/java/org/foodrev/android_example_gps_location/GpsHelpers/GpsTracker.java
@@ -0,0 +1,22 @@
+package org.foodrev.android_example_gps_location.GpsHelpers;
+
+/**
+ * Created by magulo on 10/2/16.
+ *
+ * GPS Singleton
+ */
+
+public class GpsTracker {
+
+    private static GpsTracker mGpsTracker = null;
+
+    protected GpsTracker() {}
+
+    public static GpsTracker getInstance() {
+        if(mGpsTracker == null) {
+            mGpsTracker = new GpsTracker()
+        }
+        return mGpsTracker;
+    }
+
+}
